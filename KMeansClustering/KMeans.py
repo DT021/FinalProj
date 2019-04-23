c9f9a0472119aff941123a133b45403370bb6b6e
Alex Twilla <alexandertwilla@gmail.com>
2019-04-22 20:18:07 -0400

    Finish preliminary version of KMeans algorith.

--- KMeansClustering/KMeans.py
+++ KMeansClustering/KMeans.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from sklearn import datasets
 
@@ -7,6 +8,7 @@ class KMeansCluster():
 
 	def __init__(self, data):
 		self.data = data
+		self.groupCenters = np.array([])
 
 	def addData(self, newData):
 		"""Append newData to self.data
@@ -20,9 +22,8 @@ class KMeansCluster():
 
 		self.data = newData
 
-	def cluster(self, numGroups, epochs=10000, maxRand=1, minRand=0):
-
-		groupCenters = np.random.rand(numGroups, self.data.shape[1]) * maxRand
+	def cluster(self, numGroups, epochs=10000, maxRand=1, minRand=0, logEpochs=False):
+		self.groupCenters = np.random.rand(numGroups, self.data.shape[1]) * maxRand
 		
 		for i in range(epochs):
 			groupPoints = []
@@ -35,11 +36,11 @@ class KMeansCluster():
 				distances = []
 				distance = 0
 
-				for j in range(groupCenters.shape[0]):
+				for j in range(self.groupCenters.shape[0]):
 					distance = 0
 
 					for k in range(point.shape[0]):
-						distance += (point[k] - groupCenters[j][k]) ** 2
+						distance += (point[k] - self.groupCenters[j][k]) ** 2
 
 					distance = math.sqrt(distance)
 					distances.append([j, distance])
@@ -52,8 +53,24 @@ class KMeansCluster():
 
 			# Change group centers to average of all vectors belonging to that group.
 			for j in range(numGroups):
-				break
+				points = groupPoints[j]
+				pointSum = 0
+
+				for k in range(self.data.shape[1]):
+					pointSum = 0
+
+					for point in points:
+						pointSum += point[k]
+
+					if (len(points) != 0):
+						self.groupCenters[j][k] = pointSum / len(points)
+
+	def graphData(self):
+		pass
+
+
 
 iris = datasets.load_iris()
-kmeans = KMeansCluster(iris.data)
-kmeans.cluster(3, epochs=1, maxRand=5)
+kmeans = KMeansCluster(iris.data[...,:2])
+kmeans.cluster(3, epochs=100, maxRand=5)
+kmeans.graphData()
