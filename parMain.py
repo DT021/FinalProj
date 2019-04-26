eeec69c96de3b5c6e8635b95ca437b03bfcfbe02
Alexander Twilla <alexandertwilla@gmail.com>
2019-04-26 10:58:07 -0400

    Fix issues with parMain. Update parameters.

--- parMain.py
+++ parMain.py
@@ -166,7 +166,7 @@ def parMain(fileName):
 
 #Probably should return all params as a list for easy writing to file
 	#return volParam
-fileListSP = glob.glob("Data/*.csv")
+"""fileListSP = glob.glob("Data/*.csv")
 fileListVol = glob.glob("VolData/*.csv")
 #print(parMain("studentData/test.csv"))
 # List all files in the Data Directory
@@ -174,7 +174,7 @@ fileListVol = glob.glob("VolData/*.csv")
 #fileListSP = glob.glob("../Data/*.csv")
 #fileListVol = glob.glob("../VolData/*.csv")
 
-openFile = open(r"paramsNew.txt", "a")
+openFile = open(r"paramsNew.txt", "w")
 
 #print("CHECK2")
 
@@ -198,14 +198,14 @@ for fileName in fileListSP:
     volTotal = parsSP[0]+parsSP[1]
     valTotal = parsSP[2]+parsSP[3]
 
-    if (volTotal > 4) and (valTotal > 50):
-        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2]), " ", str(parsSP[3]), " Volatile-Gain", "\n"]
-    elif (volTotal > 4) and (valTotal < 50):
-        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2]), " ", str(parsSP[3]), " Volatile-Loss", "\n"]
-    elif (volTotal < 4) and (valTotal > 50):
-        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2]), " ", str(parsSP[3]), " Stable-Gain", "\n"]
-    elif (volTotal < 4) and (valTotal < 50):
-        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2]), " ", str(parsSP[3]), " Stable-Loss", "\n"]
+    if (volTotal > 4) and (valTotal > 0):
+        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2][0]), " ", str(parsSP[3][0]), " Volatile-Gain", "\n"]
+    elif (volTotal > 4) and (valTotal <= 0):
+        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2][0]), " ", str(parsSP[3][0]), " Volatile-Loss", "\n"]
+    elif (volTotal < 4) and (valTotal > 0):
+        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2][0]), " ", str(parsSP[3][0]), " Stable-Gain", "\n"]
+    elif (volTotal < 4) and (valTotal <= 0):
+        L = [str(parsSP[0]), " ", str(parsSP[1]), " ", str(parsSP[2][0]), " ", str(parsSP[3][0]), " Stable-Loss", "\n"]
 
             #	print("L IS PRINTED\n")
             #	print(L)
@@ -229,18 +229,18 @@ for fileName in fileListVol:
         volTotal = parsVol[0]+parsVol[1]
         valTotal = parsVol[2]+parsVol[3]
 
-        if (volTotal > 4) and (valTotal > 50):
-            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2]), " ", str(parsVol[3]), " Volatile-Gain", "\n"]
-        elif (volTotal > 4) and (valTotal < 50):
-            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2]), " ", str(parsVol[3]), " Volatile-Loss", "\n"]
-        elif (volTotal < 4) and (valTotal > 50):
-            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2]), " ", str(parsVol[3]), " Stable-Gain", "\n"]
-        elif (volTotal < 4) and (valTotal < 50):
-            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2]), " ", str(parsVol[3]), " Stable-Loss", "\n"]
+        if (volTotal > 4) and (valTotal > 0):
+            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2][0]), " ", str(parsVol[3][0]), " Volatile-Gain", "\n"]
+        elif (volTotal > 4) and (valTotal <= 0):
+            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2][0]), " ", str(parsVol[3][0]), " Volatile-Loss", "\n"]
+        elif (volTotal < 4) and (valTotal > 0):
+            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2][0]), " ", str(parsVol[3][0]), " Stable-Gain", "\n"]
+        elif (volTotal < 4) and (valTotal <= 0):
+            L = [str(parsVol[0]), " ", str(parsVol[1]), " ", str(parsVol[2][0]), " ", str(parsVol[3][0]), " Stable-Loss", "\n"]
 
         openFile.writelines(L)
 
 openFile.close()
 
 sys.exit()
-#"""
+"""
